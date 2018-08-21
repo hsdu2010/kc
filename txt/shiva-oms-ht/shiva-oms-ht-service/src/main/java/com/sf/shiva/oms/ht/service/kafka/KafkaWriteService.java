657f9285b10913716a95998c4cf3b1e7ccbe3b23
huangyubo <YuBoHuang@sf-express.com>
2018-08-21 14:54:55 +0800

    ff

--- txt/shiva-oms-ht/shiva-oms-ht-service/src/main/java/com/sf/shiva/oms/ht/service/kafka/KafkaWriteService.java
+++
@@ -1,8 +0,0 @@
-package com.sf.shiva.oms.ht.service.kafka;
-
-import com.sf.shiva.oms.ht.domain.KafkaWriteManager;
-
-public interface KafkaWriteService {
-	
-	public boolean sendMsg(KafkaWriteManager kafkaWriteManager);
-}
